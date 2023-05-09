a49ab620fd56e6478d7909e8429831706475350b
danilopiccinni <danilopiccinni@gmail.com>
2023-05-09 17:52:02 +0200

    migliorato i get 'wep.php'

--- routes/web.php
+++ routes/web.php
@@ -19,57 +19,29 @@ Route::get('/', function () {
     $comics = config('comics');
     $linksNav = config('nav');
     $footerData = config('footer');
+    $signUpData = config('signUp');
+    $buyData = config('buy');
+ 
 
-    // nav
     $data = [
         'comics' => $comics,
-
+        
+        // nav
         'links' => $linksNav,
         // /nav
 
 
         // footer
         'columns' => $footerData,
-
         'copyright' => 'All Site TM and c 2020 DC Entertainment, unless otherwise noted here. All rights reserved. Cookies Setting',
-
         // /footer
 
         // SignUp
-        'socialIcons' => [
-            '/img/footer-facebook.png',
-            '/img/footer-twitter.png',
-            '/img/footer-youtube.png',
-            '/img/footer-pinterest.png',
-            '/img/footer-periscope.png',
-        ],
-
+        'socialIcons' => $signUpData,
         // /signUp
 
         // buy
-        'buyLinks' => [
-            [
-                'image' => '/img/buy-comics-digital-comics.png',
-                'title' => 'digital comics'
-            ],
-            [
-                'image' => '/img/buy-comics-merchandise.png',
-                'title' => 'dc merchandise'
-            ],
-            [
-                'image' => '/img/buy-comics-subscriptions.png',
-                'title' => 'subscription'
-            ],
-            [
-                'image' => '/img/buy-comics-shop-locator.png',
-                'title' => 'comic shop locator'
-            ],
-            [
-                'image' => '/img/buy-dc-power-visa.png',
-                'title' => 'dc power visa'
-            ],
-
-        ],
+        'buyLinks' => $buyData,
         // /buy
     ];
 
@@ -82,58 +54,24 @@ Route::get('/single-comic', function () {
     $comics = config('comics');
     $nav = config('nav');
     $footerData = config('footer');
+    $signUpData = config('signUp');
 
-    // nav
     $data = [
         'comics' => $comics,
-
+        
+        // nav
         'links' => $nav,
         // /nav
 
-
         // footer
         'columns' => $footerData,
-
         'copyright' => 'All Site TM and c 2020 DC Entertainment, unless otherwise noted here. All rights reserved. Cookies Setting',
-
         // /footer
 
         // SignUp
-        'socialIcons' => [
-            '/img/footer-facebook.png',
-            '/img/footer-twitter.png',
-            '/img/footer-youtube.png',
-            '/img/footer-pinterest.png',
-            '/img/footer-periscope.png',
-        ],
-
+        'socialIcons' => $signUpData,
         // /signUp
 
-        // buy
-        'buyLinks' => [
-            [
-                'image' => '/img/buy-comics-digital-comics.png',
-                'title' => 'digital comics'
-            ],
-            [
-                'image' => '/img/buy-comics-merchandise.png',
-                'title' => 'dc merchandise'
-            ],
-            [
-                'image' => '/img/buy-comics-subscriptions.png',
-                'title' => 'subscription'
-            ],
-            [
-                'image' => '/img/buy-comics-shop-locator.png',
-                'title' => 'comic shop locator'
-            ],
-            [
-                'image' => '/img/buy-dc-power-visa.png',
-                'title' => 'dc power visa'
-            ],
-
-        ],
-        // /buy
     ];
 
     return view('single-comic' , $data);
